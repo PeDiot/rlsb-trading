0ba029c2549ce29da435894e1a4f0be793d41640
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-04-22 12:14:47 +0200

    add plot function for eval

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import yaml
 from yaml import Loader
 
+import numpy as np
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 from stable_baselines3.common.base_class import BaseAlgorithm
-from typing import Dict, Tuple, Optional
+from typing import Dict, Tuple, Optional, List
 from src.environment import MyStocksEnv
 
 def display_env(env: MyStocksEnv, fig_dims: Tuple=(10, 4)): 
@@ -13,9 +15,23 @@ def display_env(env: MyStocksEnv, fig_dims: Tuple=(10, 4)):
     env.render_all()
     plt.show()
 
-def evaluate_episode(env: MyStocksEnv, model: Optional[BaseAlgorithm]=None) -> float: 
+def plot_results(rewards: List, profits: List, fig: Optional[plt.Figure]=None):
 
-    episode_reward = 0    
+    if fig is None:
+        fig, axes = plt.subplots(1, 2, figsize=(14, 4), facecolor="w") 
+
+    n_actions = [t+1 for t in range(len(rewards))]
+    cum_rewards = np.cumsum(rewards)
+
+    sns.lineplot(x=n_actions, y=cum_rewards, ax=axes[0])
+    sns.lineplot(x=n_actions, y=profits, ax=axes[1], color="orange")
+
+    axes[0].set_title("Cumulative Rewards")
+    axes[1].set_title("Cumulative Profits")    
+
+def evaluate_episode(env: MyStocksEnv, model: Optional[BaseAlgorithm]=None, plot: bool=False) -> float: 
+
+    rewards, profits = [], []
     observation = env.reset()
 
     while True:
@@ -25,13 +41,17 @@ def evaluate_episode(env: MyStocksEnv, model: Optional[BaseAlgorithm]=None) -> f
             action, _ = model.predict(observation)
 
         observation, reward, done, info = env.step(action)
-        episode_reward += reward
+        rewards.append(reward) 
+        profits.append(info["total_profit"])
 
         if done:
             print("info:", info)
             break
 
-    return episode_reward
+    if plot:
+        plot_results(rewards, profits)
+
+    return rewards, profits
 
 def load_config(cfg_path: str) -> Dict:
     with open(cfg_path, "r") as ymlfile:
